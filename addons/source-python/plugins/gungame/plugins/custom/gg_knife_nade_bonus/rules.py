eaed356a7d224178d0ffdd2a3547d9861037a0cd
Stephen Toon <satoon101@gmail.com>
2017-01-13 21:20:03 -0500

    Updated rules and added rules translation file.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/rules.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/rules.py
@@ -7,7 +7,6 @@
 # =============================================================================
 # GunGame
 from gungame.core.rules.instance import GunGameRules
-from gungame.core.rules.strings import rules_translations
 
 # Plugin
 from .info import info
@@ -17,3 +16,4 @@ from .info import info
 # >> RULES
 # =============================================================================
 knife_nade_bonus_rules = GunGameRules(info.name)
+knife_nade_bonus_rules.register_all_rules()
