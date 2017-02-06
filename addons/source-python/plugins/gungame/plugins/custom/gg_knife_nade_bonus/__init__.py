bca4b4351f83858c1ffd3716cb6469f14a8b3d11
Stephen Toon <satoon101@gmail.com>
2017-02-06 18:06:44 -0500

    Added module level doc-strings.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/__init__.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/__init__.py
@@ -1,3 +1,3 @@
 # ../gungame/plugins/custom/gg_knife_nade_bonus/__init__.py
 
-"""."""
+"""Players get bonuses when on knife/nade levels."""
--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
@@ -1,6 +1,6 @@
 # ../gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
 
-"""."""
+"""Plugin that gives players bonuses when on knife/nade levels."""
 
 # =============================================================================
 # >> IMPORTS
