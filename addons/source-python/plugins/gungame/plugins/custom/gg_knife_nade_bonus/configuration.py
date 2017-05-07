cbc5caf4a8f27d7e9ff7fd780d53be1f53e637aa
Stephen Toon <satoon101@gmail.com>
2017-05-06 23:04:54 -0400

    Implemented giving smoke, flash, health, speed, and color.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/configuration.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/configuration.py
@@ -16,77 +16,82 @@ from .info import info
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = (
-    'knife_color',
-    'knife_emit_smoke',
-    'knife_emit_sparks',
-    'knife_flash',
-    'knife_gravity',
-    'knife_health',
-    'knife_smoke',
-    'knife_speed',
-    'nade_color',
-    'nade_emit_smoke',
-    'nade_emit_sparks',
-    'nade_flash',
-    'nade_gravity',
-    'nade_health',
-    'nade_smoke',
-    'nade_speed',
+    'weapon_convars',
 )
 
 
 # =============================================================================
 # >> CONFIGURATION
 # =============================================================================
+weapon_convars = {
+    'knife': {},
+    'nade': {},
+}
 with GunGameConfigManager(info.name) as _config:
 
     with _config.cvar('nade_smoke') as nade_smoke:
         nade_smoke.add_text()
+        weapon_convars['nade']['smoke'] = nade_smoke
 
     with _config.cvar('nade_flash') as nade_flash:
         nade_flash.add_text()
+        weapon_convars['nade']['flash'] = nade_flash
 
     with _config.cvar('nade_health') as nade_health:
         nade_health.add_text()
+        weapon_convars['nade']['health'] = nade_health
 
     with _config.cvar('nade_gravity') as nade_gravity:
         nade_gravity.add_text()
+        weapon_convars['nade']['gravity'] = nade_gravity
 
     with _config.cvar('nade_speed') as nade_speed:
         nade_speed.add_text()
+        weapon_convars['nade']['speed'] = nade_speed
 
     with _config.cvar('nade_color') as nade_color:
         nade_color.add_text()
+        weapon_convars['nade']['color'] = nade_color
 
     with _config.cvar('nade_emit_sparks') as nade_emit_sparks:
         nade_emit_sparks.add_text()
+        weapon_convars['nade']['emit_sparks'] = nade_emit_sparks
 
     with _config.cvar('nade_emit_smoke') as nade_emit_smoke:
         nade_emit_smoke.add_text()
+        weapon_convars['nade']['emit_smoke'] = nade_emit_smoke
 
     with _config.cvar('knife_smoke') as knife_smoke:
         knife_smoke.add_text()
+        weapon_convars['knife']['smoke'] = knife_smoke
 
     with _config.cvar('knife_flash') as knife_flash:
         knife_flash.add_text()
+        weapon_convars['knife']['flash'] = knife_flash
 
     with _config.cvar('knife_health') as knife_health:
         knife_health.add_text()
+        weapon_convars['knife']['health'] = knife_health
 
     with _config.cvar('knife_gravity') as knife_gravity:
         knife_gravity.add_text()
+        weapon_convars['knife']['gravite'] = knife_gravity
 
     with _config.cvar('knife_speed') as knife_speed:
         knife_speed.add_text()
+        weapon_convars['knife']['speed'] = knife_speed
 
     with _config.cvar('knife_color') as knife_color:
         knife_color.add_text()
+        weapon_convars['knife']['color'] = knife_color
 
     with _config.cvar('knife_emit_sparks') as knife_emit_sparks:
         knife_emit_sparks.add_text()
+        weapon_convars['knife']['emit_sparks'] = knife_emit_sparks
 
     with _config.cvar('knife_emit_smoke') as knife_emit_smoke:
         knife_emit_smoke.add_text()
+        weapon_convars['knife']['emit_smoke'] = knife_emit_smoke
 
 
 '''
--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
@@ -5,3 +5,73 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Python
+from contextlib import suppress
+
+# Source.Python
+import colors
+
+# GunGame
+from gungame.core.weapons.groups import all_grenade_weapons, melee_weapons
+
+# Plugin
+from .configuration import weapon_convars
+
+
+# =============================================================================
+# >> HELPER FUNCTIONS
+# =============================================================================
+def give_bonuses(player):
+    level_weapon = player.level_weapon
+    if level_weapon not in all_grenade_weapons + melee_weapons:
+        return
+
+    weapon_type = 'nade' if level_weapon in all_grenade_weapons else 'knife'
+    convars = weapon_convars[weapon_type]
+
+    # Give smoke grenade
+    if convars['smoke'].get_bool() and level_weapon != 'smokegrenade':
+        player.give_named_item('weapon_smokegrenade')
+
+    # Give flashbang
+    if convars['flash'].get_bool() and level_weapon != 'flashbang':
+        player.give_named_item('weapon_flashbang')
+
+    # Give extra health
+    health = convars['health'].get_int()
+    if health:
+        player.health += health
+
+    # TODO: gravity
+
+    # Give extra speed
+    speed = convars['speed'].get_float()
+    if speed:
+        player.speed *= speed
+
+    # Set color
+    color = _get_color(convars['color'].get_string())
+    if color is not None:
+        player.color = color
+
+    # TODO: emit_sparks
+
+    # TODO: emit_smoke
+
+
+def _get_color(value):
+    test_value = getattr(colors, value, None)
+    if test_value is not None:
+        return test_value
+
+    with suppress(ValueError):
+        test_value = list(
+            map(
+                int,
+                value.split(',')
+            )
+        )
+        if len(test_value) in (3, 4):
+            return colors.Color(*test_value)
+
+    return None
