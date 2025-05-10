b4e217d03bc21ededed77bf882df72b35fa01c27
satoon101 <satoon101@gmail.com>
2025-05-09 20:06:43 -0400

    Updates for Ruff.
    Removed emitting sparks/smoke from knife/nade levels.

--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/configuration.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/configuration.py
@@ -11,13 +11,12 @@ from gungame.core.config.manager import GunGameConfigManager
 # Plugin
 from .info import info
 
-
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = (
-    'level_down',
-    'weapon_convars',
+    "level_down",
+    "weapon_convars",
 )
 
 
@@ -25,79 +24,29 @@ __all__ = (
 # >> GLOBAL VARIABLES
 # =============================================================================
 weapon_convars = {
-    'knife': {},
-    'nade': {},
+    "knife": {},
+    "nade": {},
 }
 
 
 # =============================================================================
 # >> CONFIGURATION
 # =============================================================================
-with GunGameConfigManager(info.name) as _config:
+with (
+    GunGameConfigManager(info.name) as _config,
+    _config.cvar(name="give_on_level_down") as level_down,
+):
+    level_down.add_text()
 
-    with _config.cvar('give_on_level_down') as level_down:
-        level_down.add_text()
+    for weapon_name in ("nade", "knife"):
+        _config.section(name=weapon_name.title())
 
-    with _config.cvar('nade_smoke') as nade_smoke:
-        nade_smoke.add_text()
-        weapon_convars['nade']['smoke'] = nade_smoke
-
-    with _config.cvar('nade_flash') as nade_flash:
-        nade_flash.add_text()
-        weapon_convars['nade']['flash'] = nade_flash
-
-    with _config.cvar('nade_health') as nade_health:
-        nade_health.add_text()
-        weapon_convars['nade']['health'] = nade_health
-
-    with _config.cvar('nade_gravity') as nade_gravity:
-        nade_gravity.add_text()
-        weapon_convars['nade']['gravity'] = nade_gravity
-
-    with _config.cvar('nade_speed') as nade_speed:
-        nade_speed.add_text()
-        weapon_convars['nade']['speed'] = nade_speed
-
-    with _config.cvar('nade_color') as nade_color:
-        nade_color.add_text()
-        weapon_convars['nade']['color'] = nade_color
-
-    with _config.cvar('nade_emit_sparks') as nade_emit_sparks:
-        nade_emit_sparks.add_text()
-        weapon_convars['nade']['emit_sparks'] = nade_emit_sparks
-
-    with _config.cvar('nade_emit_smoke') as nade_emit_smoke:
-        nade_emit_smoke.add_text()
-        weapon_convars['nade']['emit_smoke'] = nade_emit_smoke
-
-    with _config.cvar('knife_smoke') as knife_smoke:
-        knife_smoke.add_text()
-        weapon_convars['knife']['smoke'] = knife_smoke
-
-    with _config.cvar('knife_flash') as knife_flash:
-        knife_flash.add_text()
-        weapon_convars['knife']['flash'] = knife_flash
-
-    with _config.cvar('knife_health') as knife_health:
-        knife_health.add_text()
-        weapon_convars['knife']['health'] = knife_health
-
-    with _config.cvar('knife_gravity') as knife_gravity:
-        knife_gravity.add_text()
-        weapon_convars['knife']['gravity'] = knife_gravity
-
-    with _config.cvar('knife_speed') as knife_speed:
-        knife_speed.add_text()
-        weapon_convars['knife']['speed'] = knife_speed
-
-    with _config.cvar('knife_color') as knife_color:
-        knife_color.add_text()
-        weapon_convars['knife']['color'] = knife_color
-
-    with _config.cvar('knife_emit_sparks') as knife_emit_sparks:
-        knife_emit_sparks.add_text()
-        weapon_convars['knife']['emit_sparks'] = knife_emit_sparks
-
-    with _config.cvar('knife_emit_smoke') as knife_emit_smoke:
-        knife_emit_smoke.add_text()
-        weapon_convars['knife']['emit_smoke'] = knife_emit_smoke
+        for cvar in (
+            "smoke",
+            "flash",
+            "health",
+            "speed",
+        ):
+            with _config.cvar(name=f"{weapon_name}_{cvar}") as _cvar:
+                _cvar.add_text()
+                weapon_convars[weapon_name][cvar] = _cvar
--- addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_knife_nade_bonus/gg_knife_nade_bonus.py
@@ -5,14 +5,9 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
-# Python
-from contextlib import suppress
-
 # Source.Python
-import colors
 from events import Event
-from listeners import on_tick_listener_manager
-from players.entity import Player
+from filters.players import PlayerIter
 
 # GunGame
 from gungame.core.players.dictionary import player_dictionary
@@ -21,145 +16,67 @@ from gungame.core.weapons.groups import all_grenade_weapons, melee_weapons
 # Plugin
 from .configuration import level_down, weapon_convars
 
-
 # =============================================================================
-# >> CLASSES
+# >> GLOBAL VARIABLES
 # =============================================================================
-class _KnifeNadeBonusPlayer(Player):
-    """Class used to give/remove bonuses for a player."""
-
-    spark_entity = None
-
-    def __init__(self, index, weapon_type_convars):
-        """Give the player multi-level."""
-        super(_KnifeNadeBonusPlayer, self).__init__(index)
-        self.start_gravity = self.gravity
-        self.start_speed = self.speed
-        self.gravity = gravity.get_int() / 100
-        self.speed = speed.get_int() / 100
-        self.give_spark_entity()
-
-    def give_spark_entity(self):
-        """Give the player an env_spark effect."""
-        entity = self.spark_entity = Entity.create('env_spark')
-        entity.spawn_flags = 896
-        entity.angles = Vector(-90, 0, 0)
-        entity.magnitude = 8
-        entity.trail_length = 3
-        entity.set_parent(self, -1)
-        entity.origin = self.origin
-        entity.start_spark()
-
-    def remove_bonuses(self):
-        """Remove all bonuses from the player."""
-        self.gravity = self.start_gravity
-        self.speed = self.start_speed
-        self.spark_entity.stop_spark()
-        self.spark_entity.remove()
-
-
-class _MultiLevelManager(dict):
-    """Dictionary to store players with multi-level effects."""
-
-    def __delitem__(self, userid, reset_levels=True):
-        """Remove the player from the dictionary and remove the effects."""
-        if reset_levels:
-            player_dictionary[userid].multi_levels = 0
-        if userid not in self:
-            return
-        self[userid].remove_multi_level()
-        super(_MultiLevelManager, self).__delitem__(userid)
-        if not len(self):
-            on_tick_listener_manager.unregister_listener(self._tick)
-
-    def clear(self, silent=False):
-        """Remove each player individually instead of just clearing."""
-        if silent:
-            super().clear()
-            return
-        for userid in list(self):
-            del self[userid]
-
-    def delete_disconnecting_player(self, userid):
-        """Remove the disconnecting player from the dictionary."""
-        self.__delitem__(userid, reset_levels=False)
-
-    def give_bonuses(self, userid):
-        """Give the player multi-level effects."""
-        if not len(self):
-            on_tick_listener_manager.register_listener(self._tick)
-        if userid in self:
-            self.__delitem__(userid, reset_levels=False)
-        self[userid] = _KnifeNadeBonusPlayer.from_userid(userid)
-
-    def _tick(self):
-        """Reset multi-level players' gravity each tick."""
-        for player in self.values():
-            player.reset_gravity()
-
-multi_level_manager = _MultiLevelManager()
+EVENT_VARIABLES = {
+    "player_spawn": "userid",
+    "gg_level_up": "leveler",
+    "gg_level_down": "leveler",
+}
 
 
 # =============================================================================
 # >> HELPER FUNCTIONS
 # =============================================================================
-@Event('gg_level_up', 'gg_level_down')
+@Event(*EVENT_VARIABLES.keys())
 def remove_and_give_bonuses(game_event):
-    """"""
-    if game_event.name == 'gg_level_down' and not level_down.get_bool():
+    """Remove and give out bonuses."""
+    if game_event.name == "gg_level_down" and not level_down.get_bool():
         return
 
-    player = player_dictionary[game_event['leveler']]
+    player = player_dictionary[game_event[EVENT_VARIABLES[game_event.name]]]
     level_weapon = player.level_weapon
     if level_weapon not in all_grenade_weapons | melee_weapons:
         return
 
-    weapon_type = 'nade' if level_weapon in all_grenade_weapons else 'knife'
+    weapon_type = "nade" if level_weapon in all_grenade_weapons else "knife"
     convars = weapon_convars[weapon_type]
 
     # Give smoke grenade
-    if convars['smoke'].get_bool() and level_weapon != 'smokegrenade':
-        player.give_named_item('weapon_smokegrenade')
+    if convars["smoke"].get_bool() and level_weapon != "smokegrenade":
+        player.give_named_item("weapon_smokegrenade")
 
     # Give flashbang
-    if convars['flash'].get_bool() and level_weapon != 'flashbang':
-        player.give_named_item('weapon_flashbang')
+    if convars["flash"].get_bool() and level_weapon != "flashbang":
+        player.give_named_item("weapon_flashbang")
 
     # Give extra health
-    health = convars['health'].get_int()
+    health = convars["health"].get_int()
     if health:
         player.health += health
 
-    # TODO: gravity
-
     # Give extra speed
-    speed = convars['speed'].get_float()
+    speed = convars["speed"].get_float()
     if speed:
         player.speed *= speed
 
-    # Set color
-    color = _get_color(convars['color'].get_string())
-    if color is not None:
-        player.color = color
 
-    # TODO: emit_sparks
+@Event("round_freeze_end")
+def _give_speed_bonus(game_event):
+    """Give speed bonus to players on knife/nade level."""
+    speed_convars = {
+        weapon_type: weapon_convars[weapon_type]["speed"].get_float()
+        for weapon_type in ("nade", "knife")
+    }
+    for userid in [player.userid for player in PlayerIter()]:
+        player = player_dictionary[userid]
+        level_weapon = player.level_weapon
+        if level_weapon not in all_grenade_weapons | melee_weapons:
+            return
 
-    # TODO: emit_smoke
-
-
-def _get_color(value):
-    test_value = getattr(colors, value, None)
-    if test_value is not None:
-        return test_value
-
-    with suppress(ValueError):
-        test_value = list(
-            map(
-                int,
-                value.split(',')
-            )
-        )
-        if len(test_value) in (3, 4):
-            return colors.Color(*test_value)
-
-    return None
+        speed = speed_convars[
+            "nade" if level_weapon in all_grenade_weapons else "knife"
+        ]
+        if speed:
+            player.speed *= speed
